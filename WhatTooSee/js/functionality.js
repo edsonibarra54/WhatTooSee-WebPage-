dfcc798471ea6d2001c2dadf6455a8869afb6ba1
Edson Ibarra <edsonibarra54@gmail.com>
2023-11-05 21:49:10 -0600

    Added the banner carousel for the main page

--- WhatTooSee/js/functionality.js
+++ WhatTooSee/js/functionality.js
@@ -2,13 +2,47 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const second_list = document.querySelector('.second_list');
     const list = document.getElementById('ListMenu2');
-    
-    // Hacer algo con el div seleccionado (por ejemplo, agregar un evento clic)
+
+    // Inicia el carrusel de banners
+    var splide1 = new Splide('#bannerSlider', {
+        type: 'loop',
+        drag: 'free',
+        snap: true,
+        autoplay: true,
+    });
+
+    // Buscar .my-carousel-progress-bar dentro del elemento .my-carousel-progress
+    var progressContainer = document.querySelector('.my-carousel-progress');
+    var bar = progressContainer.querySelector('.my-carousel-progress-bar');
+
+    // Se modifica la barra de progreso del primer carrusel
+    splide1.on('mounted move', function () {
+        var end = splide1.Components.Controller.getEnd() + 1;
+        var rate = Math.min((splide1.index + 1) / end, 1);
+
+        if (bar) {
+            bar.style.width = String(100 * rate) + '%';
+        }
+    });
+
+    splide1.mount();
+
     menuToggle.addEventListener('click', function() {
         second_list.classList.toggle('show-menu');
-        list.style.flexDirection = 'Column';
-        //list.style.justifyContent = 'flex-end';
+        list.style.flexDirection = 'column';
     });
+
+    const dynamicDiv = document.getElementById('Banner');
+
+    // Escucha cambios en la altura del div
+    const observer = new ResizeObserver(entries => {
+        for (const entry of entries) {
+        const width = entry.contentRect.width;
+        // Ajusta la anchura basada en la altura, por ejemplo, para una relación de aspecto 4:3
+        const height = width / 2;
+        dynamicDiv.style.height = height + 'px';
+        }
+    });
+
+    observer.observe(dynamicDiv);
 });
-
-
